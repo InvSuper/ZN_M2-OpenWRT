0c425bb91c28ab958d858c4275abfa970b0ab214
Inv_Z <59054688+Super802377@users.noreply.github.com>
2026-03-16 22:42:37 +0800

    Update ZN_M2-diy-script.sh

--- ZN_M2-diy-script.sh
+++ ZN_M2-diy-script.sh
@@ -1,50 +1,15 @@
 #!/bin/bash
 
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+# 修改默认IP（保留）
+sed -i 's/192.168.100.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-# 更改默认 Shell 为 zsh
-# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+# ==========================================
+# 【核心】添加 LiBwrt 官方 Feeds 源（你截图里的 4 个有效地址）
+# ==========================================
+echo "src-git packages https://github.com/immortalwrt/packages.git" >> feeds.conf.default
+echo "src-git luci https://github.com/immortalwrt/luci.git" >> feeds.conf.default
+echo "src-git nss_packages https://github.com/LiBwrt/nss-packages.git" >> feeds.conf.default
 
-# TTYD 免登录
-# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-
-# 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-# ===================== 新增6个插件 =====================
-# 1. SmartDNS
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-
-# 2. AdGuard Home
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-
-# 3. Tailscale
-git clone --depth=1 https://github.com/immortalwrt/luci-app-tailscale package/luci-app-tailscale
-
-# 4. Turbo ACC 网络加速
-git clone --depth=1 https://github.com/chenmozhijin/turboacc package/turboacc
-
-# 5. Bandix 流量监控
-git clone --depth=1 https://github.com/brvphoenix/luci-app-bandix package/luci-app-bandix
-
-# 6. WeChatPush 微信推送
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
+# 更新并安装 Feeds（必须执行，让系统识别新的插件源）
+./scripts/feeds update -a
+./scripts/feeds install -a
