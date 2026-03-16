ebb3d111762cedcda947bc6fe254a26917f3c600
Inv_Z <59054688+Super802377@users.noreply.github.com>
2026-03-16 23:48:52 +0800

    Update ZN_M2-diy-script.sh

--- ZN_M2-diy-script.sh
+++ ZN_M2-diy-script.sh
@@ -1,21 +1,64 @@
 #!/bin/bash
 
-# --------------------------
-# 1. 修改默认IP（保留）
-# --------------------------
+# 修改默认IP
 sed -i 's/192.168.100.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-# --------------------------
-# 2. 【核心】直接覆盖 feeds.conf.default
-# --------------------------
-cat > feeds.conf.default <<'EOF'
-src-git packages https://github.com/immortalwrt/packages.git
-src-git luci https://github.com/immortalwrt/luci.git
-src-git nss_packages https://github.com/LiBwrt/nss-packages.git
-EOF
+# 更改默认 Shell 为 zsh
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
-# --------------------------
-# 3. 更新并安装 Feeds
-# --------------------------
+# TTYD 免登录
+# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 移除要替换的包
+# rm -rf feeds/packages/net/mosdns
+# rm -rf feeds/packages/net/msd_lite
+# rm -rf feeds/packages/net/smartdns
+# rm -rf feeds/luci/themes/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-netgear
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/luci/applications/luci-app-netdata
+# rm -rf feeds/luci/applications/luci-app-serverchan
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+# git_sparse_clone master https://github.com/sundaqiang/openwrt-packages luci-app-wolplus
+# git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki nikki
+# git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki luci-app-nikki
+# 添加nikki
+# add feed
+# echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+# # update & install feeds
+# ./scripts/feeds update -a
+# ./scripts/feeds install -a
+# # make package
+# make package/luci-app-nikki/compile
+
+
+# 科学上网插件
+
+# Themes
+
+
+# SmartDNS
+# git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+# git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+
+# 修改本地时间格式
+# sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+
+# 修改版本为编译日期
+# date_version=$(date +"%y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
